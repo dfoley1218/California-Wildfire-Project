a92d317230ba68adaca46302a455933cd348c957
Daniel Foley <165118764+dfoley1218@users.noreply.github.com>
2026-06-12 12:00:29 -0700

    finalized happy-path 200 tests for all endpoints, added some more test cases for the /predict endpoint

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -17,3 +17,42 @@ def test_risk(client):
     data = response.json()
     assert "risk_score" in data
     assert "factors" in data
+
+def test_fires_geojson_nearby(client):
+    response = client.get("/fires/geojson/nearby?lat=34.04&lon=-118.53&radius_km=20")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["type"] == "FeatureCollection"
+
+def test_fires_geojson_fire_name(client):
+    response = client.get("/fires/geojson/fire_name?name=PALISADES")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["type"] == "FeatureCollection"
+
+def test_fires_nearby(client):
+    response = client.get("/fires/nearby?lat=34.04&lon=-118.53&radius_km=20")
+    assert response.status_code == 200
+    data = response.json()
+
+def test_fires_largest(client):
+    response = client.get("/fires/largest?year=2025")
+    assert response.status_code == 200
+    data = response.json()
+
+def test_fires_summary(client):
+    response = client.get("/fires/summary")
+    assert response.status_code == 200
+
+def test_fires_stats_by_year(client):
+    response = client.get("/fires/stats_by_year")
+    assert response.status_code == 200
+
+def test_fires_by_name(client):
+    response = client.get("/fires/PALISADES")
+    assert response.status_code == 200
+
+def test_firms_nearby(client):
+    response = client.get("/firms/nearby?lat=34.04&lon=-118.53&radius_km=20")
+    assert response.status_code == 200
+    data = response.json()
